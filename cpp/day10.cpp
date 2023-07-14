70f22e6346cd2b96280635483d46a38b10360fad
JacobLiu <jacobliu001@qq.com>
2023-07-14 19:31:20 +0800

    Day 10: more appropriate use of emplace_back. Instead of passing a thread object and forcing a move call, pass constructor arguments and create it in the container.

--- cpp/day10.cpp
+++ cpp/day10.cpp
@@ -6,10 +6,10 @@ vector<thread> threads;
 
 template<typename F>
 void executeAfter(F f, int n) {
-    threads.emplace_back(thread([](F f1, int n1){
+    threads.emplace_back([](F f1, int n1){
         this_thread::sleep_for(chrono::milliseconds(n1));
         f1();
-    }, f, n));
+    }, f, n);
 }
 
 
